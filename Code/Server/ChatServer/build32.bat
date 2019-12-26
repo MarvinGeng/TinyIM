4abb92ba478b9cae2b35c3630699626713f129e1
DennisWin <dennismi1024@gmail.com>
2019-12-26 16:36:44 +0800

    完善Windows编译脚本

--- Code/Server/ChatServer/build32.bat
+++ Code/Server/ChatServer/build32.bat
@@ -2,4 +2,7 @@ mkdir Build64Bat
 cd Build64Bat
 cmake .. -G "Visual Studio 15 2017 Win64"
 MSBuild.exe Project.sln -property:Configuration=Debug
+cd ..
 pause
+xcopy /y /F %cd%\include\thirdparty\mysql\lib\libeay32.dll %cd%\bin\Debug\
+xcopy /y /F %cd%\include\thirdparty\mysql\lib\libmysql.dll %cd%\bin\Debug\
