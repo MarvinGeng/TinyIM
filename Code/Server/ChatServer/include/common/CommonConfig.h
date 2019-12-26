db2fb88f1c1868f7199d03adcfdee45c1c229440
DennisWin <dennismi1024@gmail.com>
2019-12-26 12:19:49 +0800

    修改版本号

--- Code/Server/ChatServer/include/common/CommonConfig.h
+++ Code/Server/ChatServer/include/common/CommonConfig.h
@@ -2,10 +2,10 @@
 #define _COMMON_CONFIG_H_
 #include <string>
 
-#ifdef WIN32
-static std::string g_SERVER_VERSION = "v0.0.1 " + std::string(__DATE__) + " " + std::string(__TIME__) + " " + std::to_string(_MSC_VER);
+#ifdef _WIN32
+static std::string g_SERVER_VERSION = "v0.1.0 " + std::string(__DATE__) + " " + std::string(__TIME__) + " " + std::to_string(_MSC_VER);
 #else
-static std::string g_SERVER_VERSION = "v0.0.1 "+ std::string(__DATE__)+" "+std::string(__TIME__)+" "+std::string(__VERSION__);
+static std::string g_SERVER_VERSION = "v0.1.0 "+ std::string(__DATE__)+" "+std::string(__TIME__)+" "+std::string(__VERSION__);
 #endif
 
 #define DEBUG 0
