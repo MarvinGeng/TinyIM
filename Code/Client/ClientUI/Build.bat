04f81004a7b4245d0b42edef964d5566f3aa1551
DennisWin <dennismi1024@gmail.com>
2019-12-27 09:48:54 +0800

    增加拷贝UI需要的文件的功能

--- Code/Client/ClientUI/Build.bat
+++ Code/Client/ClientUI/Build.bat
@@ -2,3 +2,10 @@ mkdir VS2017
 cd VS2017
 cmake ..
 MSBuild.exe TinyIM.sln
+cd ..
+copy /y %cd%\UI_RES\ %cd%\Bin\Debug\
+XCOPY /y /D /S %cd%\UI_RES\Face %cd%\Bin\Debug\Face\
+XCOPY /y /D /S %cd%\UI_RES\Image %cd%\Bin\Debug\Image\
+XCOPY /y /D /S %cd%\UI_RES\config %cd%\Bin\Debug\config\
+XCOPY /y /D /S %cd%\UI_RES\Skins %cd%\Bin\Debug\Skins\
+XCOPY /y /D /S %cd%\UI_RES\Sound %cd%\Bin\Debug\Sound\
